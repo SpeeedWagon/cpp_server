4f341ac44dd72a468bc6d125a54084c237e9808e
SpeeedWagon <gabriel.duda@deepcode.xyz>
2025-08-18 00:36:52 +0300

    cleaned code

--- cpp_server2/cpp_server2.cpp
+++ cpp_server2/cpp_server2.cpp
@@ -1,36 +1,7 @@
 #include <string>
-
 #include "WebSocketSession.h"
 #include "Listener.h"
 #include "HttpSession.h"
-
-//namespace beast = boost::beast;
-//namespace http = beast::http;
-//namespace websocket = beast::websocket;
-//namespace net = boost::asio;
-//using tcp = boost::asio::ip::tcp;
-//
-//class WebSocketSession;
-
-//http::response<http::string_body> handle_request(http::request<http::string_body>&& req) {
-//
-//    if (req.method() != http::verb::get && req.method() != http::verb::head) {
-//        http::response<http::string_body> res{ http::status::bad_request, req.version() };
-//        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-//        res.set(http::field::content_type, "text/html");
-//        res.keep_alive(req.keep_alive());
-//        res.body() = "Unknown HTTP-method";
-//        res.prepare_payload();
-//        return res;
-//    }
-//
-//    http::response<http::string_body> res{ http::status::ok, req.version() };
-//    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-//    res.set(http::field::content_type, "text/html");
-//    res.body() = "<h1>Hello from C++ Server!</h1><p>Try connecting with a WebSocket client.</p>";
-//    res.prepare_payload();
-//    return res;
-//}
 
 int main()
 {
